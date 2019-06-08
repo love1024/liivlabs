2ebb0ce48a956610fd50a52e3e5c00a8ae44ba7a
Lovepreet Singh <lovepreet.singh@nagarro.com>
2019-06-08 16:06:05 +0530

    First

---
+++ liilabs-infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace liilabs_infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
